794bbd38b43e4ec8ee4ee097aed56b2b60083d36
Xiaoxi Li <xiaoxililxx@outlook.com>
2023-09-06 14:44:32 +0200

    Added the notice of y-coordinate

--- src/main/java/main/StartGame.java
+++ src/main/java/main/StartGame.java
@@ -21,12 +21,17 @@ public class StartGame extends JPanel{
         background = ImageIO.read(new File("/Users/lxx/Library/CloudStorage/Dropbox/DIYProjects/FlappyEgg/src/main/resources/background.png"));
     }
 
+    // Draw the initial interface
+    // P.s. when we talk about the values of x and y of 2-dimensional coordinate in computer, the value of y is reversed compare to the math.
     public void paint(Graphics g) {
         // draw the background
         g.drawImage(background, 0, 0, null);
         // draw the columns
         g.drawImage(leftColumn.columnImage, leftColumn.x - leftColumn.width/2, leftColumn.y - leftColumn.height/2, null);
         g.drawImage(rightColumn.columnImage, rightColumn.x - rightColumn.width/2, rightColumn.y - rightColumn.height/2, null);
+        System.out.println("x = " + leftColumn.x + ", y = " + leftColumn.y);
+        System.out.println("Position: " + (rightColumn.x - rightColumn.width/2) + ", " + (rightColumn.y - rightColumn.height/2));
+
         // draw the gound
         g.drawImage(ground.groundImage, ground.x, ground.y, null);
         // draw the egg
@@ -38,9 +43,9 @@ public class StartGame extends JPanel{
         JFrame frame = new JFrame();
         StartGame game = new StartGame();
         frame.add(game);
-        frame.setSize(432, 644);
+        //frame.setSize(432, 644);
         // for test to show all elements, width of frame needed to set as 864
-        //frame.setSize(864, 644);
+        frame.setSize(864, 644);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
